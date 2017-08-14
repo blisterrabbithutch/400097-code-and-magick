f6835034945363c36041f2453052dc7c4951bebe
blisterrabbithutch <blisterrabbithutch@gmail.com>
2017-08-14 20:44:58 +0500

    вопросы по дополнительным условиям на второе задание

--- js/stat.js
+++ js/stat.js
@@ -19,7 +19,6 @@ window.renderStatistics = function (ctx, names, times) {
   
   var max = -1;
   var maxIndex = -1;
-
   for (var i = 0 ; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
@@ -28,6 +27,13 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
   
+  getMaxArrayElement = function(maxIndex) {
+  	return names[maxIndex];
+  	console.log(getMaxArrayElement);
+  }
+
+  console.log(getMaxArrayElement());
+
   var histogramWidth = 40;              // px;
   var histogramHeight = 140;              // px;
   var step = histogramHeight / (max - 0); // px;
@@ -37,18 +43,27 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 80);
   
   var barHeigth = 150; // px; 
-  var indent = 50;    // px;
+  var indent = 80;    // px; было 50;
   var initialX = 120; // px;
   var initialY = 250;  // px;
-  
+  var randomColorSaturation = Math.random();
+  // почему возвращает черный цвет??
+  var getRandomBlueColor = function(randomColorSaturation) {
+  	return rgba(0, 0 , 255, randomColorSaturation);
+  }
   ctx.textBaseline = 'top'; // положение надписи от левого верхнего угла
-  for(var i = 0; i < times.length; i++) {
-    if (names[i] == names[2]) {
-      ctx.fillStyle="#FF0000";
-    } else {
-      ctx.fillStyle="blue";
-    }
+  getHistorgamAndName = function(initialX, initialY, histogramWidth, step, indent) {
+  	for(var i = 0; i < times.length; i++) {
+    	if (names[i] == 'Вы') {
+      		ctx.fillStyle="#FF0000";
+    	} else {
+      		// ctx.fillStyle=histogramBlueColor();
+      		ctx.fillStyle="getRandomBlueColor()";
+    	}
     ctx.fillRect(initialX + indent * i, initialY , histogramWidth , times[i] * (- step));
     ctx.fillText(names[i], initialX + indent * i, initialY + 10);
+  	}
   }
+  getHistorgamAndName(initialX, initialY, histogramWidth, step, indent);
+  
 };
